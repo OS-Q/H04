4e415101ae4e7129a66a20ab9c4b801373299e7e
qitas <qitas@qitas.cn>
2021-02-02 15:44:59 +0800

    fix

--- examples/longan-nano-blink/src/main.c
+++ examples/longan-nano-blink/src/main.c
@@ -1,37 +1,10 @@
-/*!
-    \file  main.c
-    \brief running led
-    
-    \version 2019-6-5, V1.0.0, firmware for GD32VF103
-*/
-
-/*
-    Copyright (c) 2019, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
-*/
-
+/*******************************************************************************
+****版本：V1.0.0
+****平台：P08
+****日期：2021-02-02
+****作者：Qitas
+****版权：OS-Q
+*******************************************************************************/
 #include "gd32vf103.h"
 #include "systick.h"
 #include <stdio.h>
@@ -45,7 +18,7 @@ void longan_led_init()
 {
     /* enable the led clock */
     rcu_periph_clock_enable(LED_GPIO_CLK);
-    /* configure led GPIO port */ 
+    /* configure led GPIO port */
     gpio_init(LED_GPIO_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, LED_PIN);
 
     GPIO_BC(LED_GPIO_PORT) = LED_PIN;
@@ -60,12 +33,13 @@ void longan_led_off()
 {
     GPIO_BC(LED_GPIO_PORT) = LED_PIN;
 }
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
+
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：
+**输出参数 ：
+*******************************************************************************/
 int main(void)
 {
     longan_led_init();
@@ -79,3 +53,5 @@ int main(void)
         delay_1ms(1000);
     }
 }
+
+/*---------------------------(C) COPYRIGHT 2021 OS-Q -------------------------*/
